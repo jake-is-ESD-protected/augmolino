b66d2cd47a4e2592fbe0667f7dc968cf8c5f711f
jake-is-ESD-protected <jakob@tschavoll.at>
2022-12-19 13:37:06 +0100

    testing

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -1,6 +1,8 @@
 import os
 import librosa as lr
 import augmolino.augmentation as aug
+import numpy as np
+import numpy.testing as npt
 
 test_file = "tests/sounds/impulse_response.wav"
 
@@ -19,7 +21,7 @@ def test_augmentation_load():
     a = aug._augmentation(test_file)
     a.load()
 
-    t, _ = lr.load(test_file)
+    t, _ = lr.load(path=test_file)
 
     assert len(a.signal) == len(t)
 
@@ -34,3 +36,47 @@ def test_timeStretch_class():
     x = a.run()
 
     assert len(x) == int(len(a.signal) / test_rate)
+
+
+def test_pitchShift_class():
+
+    test_semitones = 2
+    a = aug.pitchShift(test_file, semitones=test_semitones)
+
+    assert a.descriptor == aug.descriptors[aug.__all__[1]]
+
+    x = a.run()
+
+    assert len(x) == len(a.signal)
+
+
+def test_offsetAudio_class():
+
+    test_s = 1
+    a = aug.offsetAudio(test_file, s=test_s)
+
+    assert a.descriptor == aug.descriptors[aug.__all__[2]]
+
+    x = a.run()
+
+    assert len(x) == len(a.signal) + test_s * a.sample_rate
+    npt.assert_allclose(x[0:test_s * a.sample_rate],
+                        np.zeros(test_s * a.sample_rate))
+
+    test_s = -1
+    a = aug.offsetAudio(test_file, s=test_s)
+
+    x = a.run()
+    assert len(x) == len(a.signal) + test_s * a.sample_rate
+
+
+def test_fadeAudio_class():
+
+    test_s = 1
+    test_direction = "in"
+    a = aug.fadeAudio(test_file, s=test_s,
+                      direction=test_direction)
+
+    assert a.descriptor == aug.descriptors[aug.__all__[3]]
+
+    x = a.run()
