567c531f519e64835061697f902f7f327be84af1
jake-is-ESD-protected <jt@tschavoll.at>
2022-12-23 22:40:04 +0100

    add signal return test

--- tests/test_augmenter.py
+++ tests/test_augmenter.py
@@ -1,4 +1,5 @@
 from augmolino import augmenter, augmentation
+import numpy as np
 
 
 def test_empty_augmenter_init():
@@ -11,16 +12,16 @@ def test_filled_augmenter_init():
 
     augs = [
         augmentation.pitchShift(
-            "../sounds/impulse_response.wav", semitones=1),
+            "tests/sounds/impulse_response.wav", semitones=1),
         augmentation.pitchShift(
-            "../sounds/impulse_response.wav", semitones=2)]
+            "tests/sounds/impulse_response.wav", semitones=2)]
 
     a = augmenter.augmenter(augs)
 
     assert len(a.pipe) == len(augs)
 
     for aug in a.pipe:
-        assert aug.descriptor == "pitch shift"
+        assert aug.descriptor == "pitch_shift"
         assert aug.f_source != ""
         assert aug.f_source != None
 
@@ -29,13 +30,30 @@ def test_augmenter_summary():
 
     augs = [
         augmentation.pitchShift(
-            "../sounds/impulse_response.wav", semitones=1),
+            "tests/sounds/impulse_response.wav", semitones=1),
         augmentation.pitchShift(
-            "../sounds/impulse_response.wav", semitones=2)]
+            "tests/sounds/impulse_response.wav", semitones=2)]
 
     a = augmenter.augmenter(augs)
 
     a.summary()
+
+
+def test_augmenter_run_array():
+
+    augs = [
+        augmentation.timeStretch(
+            "tests/sounds/impulse_response.wav", rate=2),
+        augmentation.pitchShift(
+            "tests/sounds/impulse_response.wav", semitones=2),
+        augmentation.offsetAudio(
+            "tests/sounds/impulse_response.wav", s=1)]
+
+    a = augmenter.augmenter(augs)
+
+    xs = a.execute()
+
+    assert len(xs) == len(augs)
 
 
 # this can only be tested visually by a human
