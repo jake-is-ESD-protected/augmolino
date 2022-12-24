b4719f14ce39085d0eaf92dc9af64e8d163dd36b
jake-is-ESD-protected <jt@tschavoll.at>
2022-12-24 16:48:37 +0100

    add dox and examples

--- augmolino/augmenter.py
+++ augmolino/augmenter.py
@@ -8,6 +8,46 @@ class augmenter:
         """
         Group class which holds a dynamic amount of 
         augmentations specified by the user
+
+        Parameters
+        ----------
+        `augmentations`:
+            Augmentation. Single or array of augmentations from
+            `augmentation.py`. If left unspecified, the augmenter
+            can later be filled with augmentations via `augmenter.add()`.
+            Default is `None`.
+
+        Notes
+        -----
+        Depending on the `f_dest`-path of every augmentation the augmenter
+        returns either an array of augmented signals from each augmentation
+        or an array of `None` for each augmentation. If a location is 
+        specified, the signals get stored as `.wav`-files. See "Examples"
+        for more info.
+
+        Examples
+        --------
+        >>> # get augmentations as signals and plot them:
+        >>> from augmolino import augmenter, augmentation
+        >>> import matplotlib.pyplot as plt
+        >>> # specify set of augmentations:
+        >>> augs = [
+                augmentation.timeStretch(
+                    "tests/sounds/impulse_response.wav", rate=2),
+                augmentation.pitchShift(
+                    "tests/sounds/impulse_response.wav", semitones=2),
+                augmentation.offsetAudio(
+                    "tests/sounds/impulse_response.wav", s=1)]
+        >>> # create the augmenter
+        >>> a = augmenter.augmenter(augs)
+        >>> # run augmenter
+        >>> xs = a.execute()
+        >>> # create plot
+        >>> fig, axs = plt.subplots(3,1)
+        >>> # display signals
+        >>> for i, x in enumerate(xs):
+            >>> axs[i].plot(x)
+        >>> plt.show()
         """
 
         if not augmentations:
@@ -22,9 +62,31 @@ class augmenter:
                 self.pipe = [augmentations]
 
     def add(self, augmentation):
+        """
+        Add a single augmentation to the augmenter pipeline.
+
+        Parameters
+        ----------
+        `augmentation`:
+            Augmentation. Gets appended to existing or empty
+            pipe of augmentations within the augmenter.
+        """
         self.pipe.append(augmentation)
 
     def execute(self):
+        """
+        Run all augmentations within the pipe. Specific settings are
+        inside of each augmentation.
+
+        Returns
+        -------
+        `xs`:
+            Array. Returns each augmented signal if no save location
+            has been specified for the coresponding augmentation.
+            Otherwise it returns an array of shape (n_augmentations, )
+            filled with single `None` values.
+
+        """
         # this is sloooow but the only way to append dynamic sizes
         xs = [[]] * len(self.pipe)
         for i, augmentation in enumerate(self.pipe):
@@ -32,7 +94,10 @@ class augmenter:
 
             if augmentation.f_dest == None:
                 xs[i].append(x)
-                xs[i] = np.asarray(xs[i][i])
+
+            else:
+                xs[i].append(None)
+            xs[i] = np.asarray(xs[i][i])
 
         return xs
 
