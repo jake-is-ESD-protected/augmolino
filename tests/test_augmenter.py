3831baeef15c4953afccc345ce576a26ab8ad288
jake-is-ESD-protected <jts4681@students.fhv.at>
2023-01-22 00:04:02 +0100

    add tests for new saving styles

--- tests/test_augmenter.py
+++ tests/test_augmenter.py
@@ -2,6 +2,9 @@ from augmolino import augmenter, augmentation
 import numpy as np
 import os
 
+source_dir = "tests/sounds/"
+source_file = "tests/sounds/speech.wav"
+
 
 def test_empty_augmenter_init():
 
@@ -12,10 +15,8 @@ def test_empty_augmenter_init():
 def test_filled_augmenter_init():
 
     augs = [
-        augmentation.pitchShift(
-            "tests/sounds/impulse_response.wav", semitones=1),
-        augmentation.pitchShift(
-            "tests/sounds/impulse_response.wav", semitones=2)]
+        augmentation.pitchShift(semitones=1),
+        augmentation.pitchShift(semitones=2)]
 
     a = augmenter.augmenter(augs)
 
@@ -23,17 +24,13 @@ def test_filled_augmenter_init():
 
     for aug in a.pipe:
         assert aug.descriptor == "pitch_shift"
-        assert aug.f_source != ""
-        assert aug.f_source != None
 
 
 def test_augmenter_summary():
 
     augs = [
-        augmentation.pitchShift(
-            "tests/sounds/impulse_response.wav", semitones=1),
-        augmentation.pitchShift(
-            "tests/sounds/impulse_response.wav", semitones=2)]
+        augmentation.pitchShift(semitones=1),
+        augmentation.pitchShift(semitones=2)]
 
     a = augmenter.augmenter(augs)
 
@@ -43,46 +40,79 @@ def test_augmenter_summary():
 def test_augmenter_run_array():
 
     augs = [
-        augmentation.timeStretch(
-            "tests/sounds/impulse_response.wav", rate=2),
-        augmentation.pitchShift(
-            "tests/sounds/impulse_response.wav", semitones=2),
-        augmentation.offsetAudio(
-            "tests/sounds/impulse_response.wav", s=1)]
+        augmentation.timeStretch(rate=2),
+        augmentation.pitchShift(semitones=2),
+        augmentation.offsetAudio(s=1)]
 
     a = augmenter.augmenter(augs)
 
-    xs = a.execute()
+    xs = a.execute(source_file)
 
     assert len(xs) == len(augs)
 
 
-def test_augmenter_run_save():
+def test_augmenter_run_single_save():
 
     fnames = ["targetfile1.wav",
               "targetfile2.wav",
               "targetfile3.wav"]
 
     augs = [
-        augmentation.timeStretch(
-            "tests/sounds/impulse_response.wav", fnames[0], rate=2),
-        augmentation.pitchShift(
-            "tests/sounds/impulse_response.wav", fnames[1], semitones=2),
-        augmentation.offsetAudio(
-            "tests/sounds/impulse_response.wav", fnames[2], s=1)]
+        augmentation.timeStretch(rate=2),
+        augmentation.pitchShift(semitones=2),
+        augmentation.offsetAudio(s=1)]
 
     a = augmenter.augmenter(augs)
 
-    xs = a.execute()
-
-    assert xs == [[None]] * len(fnames)
-    print(xs)
+    for fname in fnames:
+        xs = a.execute(source_file, fname)
+        assert xs == None
 
     for fname in fnames:
         assert os.path.exists(fname)
         os.remove(fname)
 
 
+def test_augmenter_run_multi_save():
+
+    fnames = ["targetfile1.wav",
+              "targetfile2.wav",
+              "targetfile3.wav"]
+
+    augs = [augmentation.timeStretch(rate=2),
+            augmentation.pitchShift(semitones=2),
+            augmentation.offsetAudio(s=1)]
+
+    a = augmenter.augmenter(augs)
+
+    for fname in fnames:
+        xs = a.execute(source_file, fname)
+        assert xs == None
+
+    for fname in fnames:
+        assert os.path.exists(fname)
+        os.remove(fname)
+
+def test_augmenter_run_folder_auto_save():
+
+    augs = [augmentation.timeStretch(rate=2),
+            augmentation.pitchShift(semitones=2),
+            augmentation.offsetAudio(s=1)]
+
+    a = augmenter.augmenter(augs)
+
+    xs = a.execute(source_dir, dest="auto")
+    assert xs == None
+
+def test_cleanup():
+    keep = ["impulse_response.wav",
+            "speech.wav"]
+
+    for file in os.listdir(source_dir):
+        if file not in keep:
+            os.remove(os.path.join(source_dir, file))
+
 # this can only be tested visually by a human
 test_augmenter_summary()
-test_augmenter_run_save()
+# clean up all augmented files within the source dir
+test_cleanup()
