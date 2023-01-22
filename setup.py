260da566bbef237b57ef0cd128abfcc5a9217e95
jake-is-ESD-protected <jts4681@students.fhv.at>
2023-01-22 15:38:25 +0100

    ready for new release

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='augmolino',
       packages=['augmolino'],
-      version='0.0.1a',
+      version='0.0.2',
       description='augmentation for audio based datasets for machine learning',
       url='https://github.com/jake-is-ESD-protected/augmolino',
-      download_url="https://github.com/jake-is-ESD-protected/augmolino/archive/refs/tags/0.0.1.tar.gz",
+      download_url="https://github.com/jake-is-ESD-protected/augmolino/archive/refs/tags/0.0.2.tar.gz",
       author='Jakob Tschavoll',
       author_email='jt@tschavoll.at',
       license='GPL 3.0',
